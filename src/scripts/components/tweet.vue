77231a2f871a48658ac25a17cdd0fb6287d937b4
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-23 18:19:31 +0200

    formatowanie tweetow i wiecej informacji

--- src/scripts/components/tweet.vue
+++ src/scripts/components/tweet.vue
@@ -1,5 +1,12 @@
 <template>
-    <b-card class="m-3">
+    <b-card
+        :title="tweet.author.name"
+        :sub-title="tweet.created_time | myDate"
+        :img-src="tweet.author.avatar_url"
+        :img-alt="tweet.author.name"
+        img-left
+        class="m-3"
+    >
         <b-card-text>
             {{ tweet.body }}
         </b-card-text>
@@ -7,12 +14,25 @@
 </template>
 
 <script>
+import { myDate } from '@/helpers/my-date'
 export default {
     name: "Tweet",
     props: {
         tweet: {
             id: String,
-            body: String
+            body: String,
+            author: Object,
+            created_time: String
+        }
+    },
+    computed: {
+        created_time() {
+            return myDate(this.tweet.created_time);
+        }
+    },
+    filters: {
+        myDate(value) {
+            return myDate(value);
         }
     }
 }
---
+++ src/scripts/helpers/my-date.js
@@ -0,0 +1,4 @@
+export function myDate(value) {
+    const d = new Date(value);
+    return d.toLocaleString('pl-PL');
+}
