25aa0705a5d359315558f0cf3b1f30303673b097
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-23 13:02:32 +0200

    testowanie app

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -2,10 +2,20 @@
 
 import { assert } from 'chai';
 
+import App from "@/components/app";
+import TweetList from "@/components/tweet-list";
+
+import ( mount ) from '@vue/test-utils'
+
 suite('App', () => {
-    it('should plus 2 and 2', () => {
+    it.skip('should plus 2 and 2', () => { //.skip mówi MOCHA by pominąć
         const input = 2 + 2;
         const output = 4;
         assert.equal(input, output);
+    });
+
+    it('should render single TweetList item', () => {
+        const wrapper = mount(App);
+        assert.ok(wrapper.contains(TweetList));
     });
 });
