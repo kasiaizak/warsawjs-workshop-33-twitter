2eade8d1e6d3ae45ef85faf24498607cc8e3ada8
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-23 13:02:50 +0200

    testowanie tweet-list

---
+++ test/specs/tweet-list.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+import { assert } from 'chai';
+
+import TweetList from "@/components/tweet-list";
+import Tweet from "@/components/tweet";
+
+import(mount) from '@vue/test-utils'
+
+suite('TweetList', () => {
+    it('should render single Tweet item', () => {
+        const wrapper = mount(TweetList);
+        assert.ok(wrapper.contains(Tweet));
+    });
+});
