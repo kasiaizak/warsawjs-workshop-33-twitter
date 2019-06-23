8cbce65e4c23a773b72b8fcc2ac84d3a6f090311
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-23 12:49:36 +0200

    testy app

---
+++ test/specs/app.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+import { assert } from 'chai';
+
+suite('App', () => {
+    it('should plus 2 and 2', () => {
+        const input = 2 + 2;
+        const output = 4;
+        assert.equal(input, output);
+    });
+});
