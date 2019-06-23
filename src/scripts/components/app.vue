06e0861a7922b035d9bb51af2f4e8962d79e01c7
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-23 17:03:14 +0200

    pobieranie tweetow z lokalnego serwera json

--- src/scripts/components/app.vue
+++ src/scripts/components/app.vue
@@ -6,6 +6,7 @@
 
 <script>
 import TweetList from "@/components/tweet-list";
+import { request } from 'http';
 
 export default {
     name: "App",
@@ -15,11 +16,28 @@ export default {
     data() {
         return {
             tweets: [
-                {id: "sdkahkasd", body: "fahby trener!"},
-                {id: "sddeweer", body: "kolejna linika"},
-                {id: "dsdsda", body: "i jeszcze jedna"}
+                // http://localhost:3000/tweets
             ]
         }
+    },
+    methods: {
+        async fetchTweets() {
+            const url = 'http://localhost:3000/tweets';
+            try {
+                // 1. make HTTP request
+                const response = await fetch(url);
+                // 2. parse to JSON
+                return await response.json();
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    },
+
+    async mounted() {
+        console.log('app.vue is ready');
+        // 3. assign to this.tweets
+        this.tweets = await this.fetchTweets();
     }
 }
 </script>
--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -4,23 +4,42 @@ import { assert } from 'chai';
 
 import App from "@/components/app";
 import TweetList from "@/components/tweet-list";
+import nock from "nock";
 
 import { mount } from '@vue/test-utils';
 
 suite('App', () => {
-    it.skip('should plus 2 and 2', () => { //.skip mówi MOCHA by pominąć
-        const input = 2 + 2;
-        const output = 4;
-        assert.equal(input, output);
-    });
-
     it('should render single TweetList item', () => {
         const wrapper = mount(App, {
             stubs: {
-                "b-card": true,
+                "b-card": '<p>Lorem ipsum</p>',
                 "b-card-text": true
             }
         });
         assert.ok(wrapper.contains(TweetList));
+    });
+
+    it.skip('should fetch tweets via HTTP request', async () => { //.skip mówi MOCHA by pominąć
+        const wrapper = mount(App, {
+            stubs: {
+                Tweet: true
+            }
+        });
+
+        // @ts-ignore
+        assert.isFunction(wrapper.vm.fetchTweets);
+        
+        // @ts-ignore
+        const ft = wrapper.vm.fetchTweets;
+
+        nock("http://localhost:3000")
+            .get('/tweets')
+            .reply(200, [
+                { id: 'id', body: 'body' }
+            ]);
+
+        const response = await ft();
+        console.log(response); // JSON
+        assert.lengthOf(response, 1);
     });
 });
