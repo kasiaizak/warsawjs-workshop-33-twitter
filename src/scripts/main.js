ea863ca0654b80c141ea8666f58eeb17a4d299cb
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-23 12:10:03 +0200

    komponent App

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 
 
-import Tweet from '@/components/tweet';
+import App from '@/components/app';
 
 import Vue from 'vue';
 
+// eslint-disable-next-line no-new
 new Vue({
     el: '#app',
     components: {
-        Tweet
+        App
     }
 });
